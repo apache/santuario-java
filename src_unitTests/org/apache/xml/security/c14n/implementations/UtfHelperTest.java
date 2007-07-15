69198d5941f8fb1af81272443d6e0d4f87eb3898
Raul Benito Garcia <raul@apache.org>
2007-07-15 15:58:17 +0000

    New Branch for Stax work
    git-svn-id: https://svn.apache.org/repos/asf/xml/security/branches/stax_jsr105@556421 13f79535-47bb-0310-9956-ffa450edef68

--- src_unitTests/org/apache/xml/security/c14n/implementations/UtfHelperTest.java
+++ src_unitTests/org/apache/xml/security/c14n/implementations/UtfHelperTest.java
@@ -1,18 +1,8 @@
 package org.apache.xml.security.c14n.implementations;
 
-import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
-
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.apache.xml.security.c14n.implementations.UtfHelpper;
-import org.apache.xml.security.signature.XMLSignatureInput;
-import org.apache.xml.security.transforms.Transforms;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -28,6 +18,35 @@ public class UtfHelperTest extends TestCase {
 			assertTrue(equals);
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	public void testUtf() throws Exception {
+		int chunk=1<<16; int j=0;
+		ByteArrayOutputStream charByCharOs=new ByteArrayOutputStream();
+		ByteArrayOutputStream strOs=new ByteArrayOutputStream();
+		
+		char chs[]=new char[chunk];
+		for (int i=0;i<chunk; i++) {
+			int ch=(chunk*j)+i;
+			if (ch==0xDBFF) {
+				ch=1;
+			}
+			chs[i]=(char)ch;
+			UtfHelpper.writeCharToUtf8((char) ch, charByCharOs);
+		}
+		String str=new String(chs);
+		byte a[]=UtfHelpper.getStringInUtf8(str);
+		try {
+			System.out.println("chunk:"+j);
+			byte correct[]=str.getBytes("UTF8");
+			assertTrue("UtfHelper.getStringInUtf8 failse",Arrays.equals(correct, a));
+			assertTrue("UtfHelper.getStringInUtf8 failse",Arrays.equals(correct, charByCharOs.toByteArray()));
+			UtfHelpper.writeStringToUtf8(str, strOs);
+			assertTrue("UtfHelper.writeStringToUtf8 failse",Arrays.equals(correct, strOs.toByteArray()));
+			
+		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
 		
